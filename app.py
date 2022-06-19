1336edd8a6fb6898ec717a7180334cf957e8e670
unknown <itsalexwilliams69@gmail.com>
2022-06-19 00:16:07 -0700

    fixed

--- app.py
+++ app.py
@@ -8,10 +8,7 @@ from colorama import Fore, Style
 
 
 #soem global shit
-player = "mpv"
 lmagenta = lambda a: f"{Fore.LIGHTMAGENTA_EX}{a}{Style.RESET_ALL}"
-
-
 
 provider = ask_provider()
 
@@ -43,25 +40,21 @@ if provider == "gogoplay":
 
         quality = ask_quality(qualities)
         final_link = links[qualities.index(quality)]
-        #print(final_link)
-        play_mpv([{"stream_url":final_link,"audio":audio,"referrer":"https://goload.pro","name":anime_name}])
-        #os.system(f'{player} --referrer="https://goload.pro" {final_link}' if audio==None else f'{player} --referrer="https://goload.pro" --audio-file="{audio}" "{final_link}"')
+        play_mpv([{"stream_url":final_link,"audio":audio,"referrer":"https://goload.pro","name":f"{anime_name}_{p}"}])
+
     except:
         print(red("[*]Change provider"))
 
 else:
     data = extract_episode_info(anime_to_watch)
+    
     p = input(lmagenta(f"[*]Enter episode(total {data['eptotal']}): "))
+    
     links = data[str(int(p)-1)]
-    #print(links)
+    
     data = generate_link(links)[0]
     
-    #referrer,link = al_return_link(x)
-    
-    #os.system(f'{player} --referrer="{referrer}" "{link}"' if len(referrer)>0 else f'{player} "{link}"')
-    #os.system(f'{player} "{link}"' if audio==None else f'{player} --audio-file="{audio}" "{link}"')
-    
-    #append name in the data
     data["name"] = f"{anime_name}_{p}"
+    
     play_mpv([data])
     
